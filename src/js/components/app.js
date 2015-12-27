a56b2b322f73860fabe7e45fd69475b772d622bc
armando <armandomendivil.m@gmail.com>
2015-12-27 09:39:33 -0800

    setup

---
+++ src/js/components/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+ * Import modules
+ */
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>Hello React JS</div>
+    );
+  }
+}
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+ * Import Modules
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+ReactDOM.render(<App />, document.getElementById('main'));
